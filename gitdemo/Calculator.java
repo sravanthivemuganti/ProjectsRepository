b838bf64a365f0e923cb73fc10a49678f926d939
sravanthi <vgs.godha@gmail.com>
2019-09-29 21:35:26 -0700

    Adding new file

---
+++ gitdemo/Calculator.java
@@ -0,0 +1,10 @@
+package com.vgs.test.samples;
+
+public class Demo {
+public static void main(String[] args) {
+	System.out.println("demo class");
+	System.out.println("Hello");
+	System.out.println("Java");
+	System.out.println("");
+}
+}
